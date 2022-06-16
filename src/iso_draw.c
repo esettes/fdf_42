6566d1b7c8254ef36d774412a7d8289d4a22aa2f
Ioana roxana Stancu <iostancu@c3r5s3.42madrid.com>
2022-06-16 20:31:59 +0200

    a part of segments are in isometric angle

--- src/iso_draw.c
+++ src/iso_draw.c
@@ -6,7 +6,7 @@
 /*   By: iostancu <iostancu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/10 15:27:12 by iostancu          #+#    #+#             */
-/*   Updated: 2022/06/16 19:28:05 by iostancu         ###   ########.fr       */
+/*   Updated: 2022/06/16 20:30:32 by iostancu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ double	get_iso_pos_x(t_mtrx mtrx)
 	size.x = mtrx.segments.x / 2;
 	size.y = mtrx.segments.y / 2;
 	pos_x = (size.x) * cos(30) - (size.y * sin(30));
-	pos_x += IMG_CENTER_X;
+	pos_x = IMG_CENTER_X + pos_x - mtrx.px_size.x;
 	return (pos_x);
 }
 
@@ -41,6 +41,6 @@ double	get_iso_pos_y(t_mtrx mtrx)
 	size.x = mtrx.segments.x / 2;
 	size.y = mtrx.segments.y / 2;
 	pos_y = (size.x) * sin(30) + (size.y * cos(30));
-	pos_y += IMG_CENTER_Y;
+	pos_y = IMG_CENTER_Y + pos_y - mtrx.px_size.y;
 	return (pos_y);
 }
