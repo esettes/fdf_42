11f6b98ae5d211f8c4ed72bd3e43061048687f9c
Ioana roxana Stancu <iostancu@c2r14s5.42madrid.com>
2022-06-17 20:32:05 +0200

    draw something like a top wall

--- src/draw.c
+++ src/draw.c
@@ -67,48 +67,126 @@ void	draw_outer_segments(t_fdf *fdf)
 
 void	test_draw_iso(t_fdf *fdf)
 {
-	t_vec2	new_start;
-
-	t_vec2	new_end;
+	t_vec2	start;
+	t_vec2	end;
 	t_vec2	seg_iter;
+
+	t_vec2	img_size;
 
 	seg_iter.x = 0;
 	seg_iter.y = 0;
+	start.x = (int)fdf->mtrx.start_draw.x;
+	start.y = (int)fdf->mtrx.start_draw.y;
+	end.x = (int)fdf->mtrx.end_draw.x;
+	end.y = (int)fdf->mtrx.start_draw.y;
 
-	new_start = set_mesh_iso(fdf);
-	new_start.y = (int)fdf->mtrx.start_draw.y;
-	new_end = set_mesh_iso(fdf);
-	new_end.x += fdf->mtrx.px_size.x;
-	new_end.y += fdf->mtrx.px_size.y ;
-
+	img_size.x = (int)fdf->img->width;
+	img_size.y = (int)fdf->img->height;
 	while (seg_iter.y < fdf->mtrx.segments.y)
 	{
-		/* Dejar coordenadas tal cual salen de set_mesh_iso
-		Luego "partir" por la mitad el cuadrado, y hacer un corte desde la esquina derecha hasta
-		el vertice donde se corta la parte de abajo.
-		lo que sobra, colocarlo por orden en la esquina superior izquierda, creando así la perspectiva*/
-		new_start.x += (fdf->mtrx.zoom ) * sin(30);// - (fdf->mtrx.segments.y / 2 * sin(30));
-		new_start.y += (fdf->mtrx.zoom) * cos(30);
-		new_end.x += (fdf->mtrx.zoom ) * sin(30);
-		new_end.y += (fdf->mtrx.zoom) * cos(30);
-		draw_segment_horiz(new_start, new_end, fdf, 0.6);
+		
+		start.x -= fdf->mtrx.zoom * cos (45);// + fdf->mtrx.px_size.x; //* (fdf->mtrx.px_size.x * cos(30));
+		start.y += fdf->mtrx.zoom * sin(-45) * 0.5;
+		end.x += fdf->mtrx.zoom * cos (45) * 0.5;
+		end.y -= fdf->mtrx.zoom * sin (45);
+		draw_segment_horiz(start, end, fdf, 0);
 		seg_iter.y++;
 	}
 	
-	new_start.y = round(fdf->mtrx.start_draw.y);
-	new_end.y = new_start.y + fdf->mtrx.px_size.y;
-	
+	start.x = (int)fdf->mtrx.start_draw.x;
+	start.y = (int)fdf->mtrx.start_draw.y;
+	end.x = (int)fdf->mtrx.end_draw.x;
+	end.y = (int)fdf->mtrx.start_draw.y;
+	start.y = round(fdf->mtrx.start_draw.y);
+	end.y = start.y + fdf->mtrx.px_size.y;
 	while (seg_iter.x < fdf->mtrx.segments.x)
 	{
-		draw_segment_vert(new_start, new_end, fdf, 0);
-		// new_start.x += fdf->mtrx.zoom;
-		// new_end.x += fdf->mtrx.zoom;
-		new_start.x -= (fdf->mtrx.zoom ) * sin(30);// - (fdf->mtrx.segments.y / 2 * sin(30));
-		new_start.y -= (fdf->mtrx.zoom) * cos(30);
-		new_end.x -= (fdf->mtrx.zoom ) * sin(30);
-		new_end.y -= (fdf->mtrx.zoom) * cos(30);
+		draw_segment_vert(start, end, fdf, 0.6);
+		start.x += fdf->mtrx.zoom;
+		end.x += fdf->mtrx.zoom;
 		seg_iter.x++;
 	}
+
+
+	// t_vec2	start;
+	// t_vec2	end;
+	// t_vec2	seg_iter;
+
+	// t_vec2	img_size;
+	// t_vec2	size;
+	// t_vec2	pos;
+
+	// seg_iter.x = 0;
+	// seg_iter.y = 0;
+	
+	// size.x = fdf->mtrx.segments.x / 2;
+	// size.y = fdf->mtrx.segments.y / 2;
+	// pos.x = (size.x) * cos(30) - (size.y * sin(30));
+	// pos.x = IMG_CENTER_X + pos.x - fdf->mtrx.px_size.x;
+	// pos.y = (size.x) * sin(30) + (size.y * cos(30));
+	// pos.y = IMG_CENTER_Y + pos.y - fdf->mtrx.px_size.y;
+
+	// //start = set_mesh_iso(fdf);
+	// start.x = (int)fdf->mtrx.start_draw.x;
+	// start.y = (int)fdf->mtrx.start_draw.y;
+	// //end = set_mesh_iso(fdf);
+	// end.x += fdf->mtrx.px_size.x;
+	// end.y += fdf->mtrx.px_size.y ;
+
+
+	// img_size.x = (int)fdf->img->width;
+	// img_size.y = (int)fdf->img->height;
+	// while (seg_iter.y < fdf->mtrx.segments.y)
+	// {
+	// 	draw_segment_horiz(start, end, fdf, 0);
+	// 	start.x -= (size.x) * cos(30);
+	// 	start.y += fdf->mtrx.zoom / sin (30);
+	// 	end.x -= (size.y * cos(30));
+	// 	end.y += fdf->mtrx.zoom / sin (30);
+	// 	seg_iter.y++;
+	// }
+	// start.y = round(fdf->mtrx.start_draw.y);
+	// end.y = start.y + fdf->mtrx.px_size.y;
+	// while (seg_iter.x < fdf->mtrx.segments.x)
+	// {
+	// 	draw_segment_vert(start, end, fdf, 0.6);
+	// 	start.x += fdf->mtrx.zoom ;
+	// 	//start.y += fdf->mtrx.zoom;
+	// 	end.x += fdf->mtrx.zoom;
+	// 	//end.y += fdf->mtrx.zoom ;
+	// 	seg_iter.x++;
+	// }
+
+
+
+	// while (seg_iter.y < fdf->mtrx.segments.y)
+	// {
+	// 	/* Dejar coordenadas tal cual salen de set_mesh_iso
+	// 	Luego "partir" por la mitad el cuadrado, y hacer un corte desde la esquina derecha hasta
+	// 	el vertice donde se corta la parte de abajo.
+	// 	lo que sobra, colocarlo por orden en la esquina superior izquierda, creando así la perspectiva*/
+	// 	//new_start.x += (fdf->mtrx.zoom ) * sin(30);// - (fdf->mtrx.segments.y / 2 * sin(30));
+	// 	//new_start.y += (fdf->mtrx.zoom);// * cos(30);
+	// 	//new_end.x += (fdf->mtrx.zoom ) * sin(30);
+	// 	//new_end.y += (fdf->mtrx.zoom);// * cos(30);
+	// 	new_start
+
+
+	// 	draw_segment_horiz(new_start, new_end, fdf, 0.6);
+	// 	seg_iter.y++;
+	// }
+	// new_start = set_mesh_iso(fdf);
+	// new_start.y = round(fdf->mtrx.start_draw.y);
+	// new_end.y = new_start.y + fdf->mtrx.px_size.y;
+	// while (seg_iter.x < fdf->mtrx.segments.x)
+	// {
+	// 	draw_segment_vert(new_start, new_end, fdf, 0);
+	// 	new_start.x += fdf->mtrx.zoom * sin(30);
+	// 	new_start.y += fdf->mtrx.zoom * cos(30);
+	// 	new_end.x += fdf->mtrx.zoom * sin(30);
+		
+	// 	seg_iter.x++;
+	// }
 }
 
 void	draw_segment_horiz(t_vec2 start, t_vec2 end, t_fdf *fdf, double color)
