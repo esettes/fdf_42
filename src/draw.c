aa931c2065201fa4bda3cb9ca36032417b930121
esettes <ioanaroxana.st@gmail.com>
2022-06-22 15:36:19 +0100

    merge

--- src/draw.c
+++ src/draw.c
@@ -54,11 +54,7 @@ void	view_on_top(t_fdf *fdf)
 	end.y = start.y + fdf->mtrx.px_size.y;
 	while (seg_iter.x < fdf->mtrx.segments.x)
 	{
-<<<<<<< HEAD
-		draw_segment_negative(start, end, fdf, 0);
-=======
 		draw_segment_to_left(start, end, fdf, 0);
->>>>>>> 9830de97052b839d1fb4868a4aea3da1fd34b158
 		start.x += fdf->mtrx.zoom;
 		end.x += fdf->mtrx.zoom;
 		seg_iter.x++;
@@ -71,31 +67,12 @@ void	test_view_iso(t_fdf *fdf)
 	t_vec2	end;
 	t_vec2	seg_iter;
 	t_vec2 	aux;
-<<<<<<< HEAD
-	// t_vec2	save;
-//	t_vec2	aux_vert;
-=======
 	t_vec2	zoom_iter;
 	t_iso_distorsion	iso_dist;
->>>>>>> 9830de97052b839d1fb4868a4aea3da1fd34b158
 
 	seg_iter.x = 0;
 	seg_iter.y = 0;
 	
-<<<<<<< HEAD
-	aux.x = (IMG_CENTER_X - (int)fdf->mtrx.start_draw.x) * 0.99;
-	aux.y = (IMG_CENTER_Y - (int)fdf->mtrx.start_draw.y) * 0.99;
-
-	start.x = (int)fdf->mtrx.start_draw.x + aux.x;
-	start.y = (int)fdf->mtrx.start_draw.y;
-
-	// save.x = start.x;
-	// save.y = start.y;
-	end.x = (int)fdf->mtrx.end_draw.x + aux.x;
-	end.y = (int)fdf->mtrx.start_draw.y + aux.y;
-
-	while (seg_iter.y < fdf->mtrx.segments.y)
-=======
 	aux.x = (IMG_CENTER_X - fdf->mtrx.start_draw.x) * 1;
 	aux.y = (IMG_CENTER_Y - fdf->mtrx.start_draw.y) * 1;
 	start.x = fdf->mtrx.start_draw.x + aux.x;
@@ -108,30 +85,10 @@ void	test_view_iso(t_fdf *fdf)
 	iso_dist.y = 0.5;
 	while(seg_iter.y < fdf->mtrx.segments.y)
 //	while (seg_iter.y <= fdf->mtrx.px_size.y)
->>>>>>> 9830de97052b839d1fb4868a4aea3da1fd34b158
 	{
 		if (seg_iter.y < 2)
 			draw_segment_to_right(start, end, fdf, 4);
 		if (seg_iter.y >= 2)
-<<<<<<< HEAD
-			draw_segment_horiz(start, end, fdf, 0);
-		start.x -= fdf->mtrx.zoom * cos(45) * 1.9;// + fdf->mtrx.px_size.x; //* (fdf->mtrx.px_size.x * cos(30));
-		start.y += fdf->mtrx.zoom * sin(45) * 0.6;
-		end.x -= fdf->mtrx.zoom * cos(45) * 1.9;
-		end.y += fdf->mtrx.zoom * sin(45) * 0.6;
-		
-		seg_iter.y++;
-	}
-//	aux_vert.x = (IMG_CENTER_X - (int)fdf->mtrx.end_draw.x) * 0.99;
-//	aux_vert.y = (IMG_CENTER_Y - (int)fdf->mtrx.start_draw.y) * 0.99;
-
-	//start.x = (int)fdf->mtrx.start_draw.x + aux.x;
-	//start.y = (int)fdf->mtrx.start_draw.y ;
-	// end.x = (int)fdf->mtrx.end_draw.x - aux_vert.x;
-	// end.y = (int)fdf->mtrx.start_draw.y + aux_vert.y;
-	end.x = (int)fdf->mtrx.start_draw.x + aux.x;
-	end.y = (int)fdf->mtrx.start_draw.y - aux.y;
-=======
 			draw_segment_to_right(start, end, fdf, 0);
 		start.x -= zoom_iter.y * cos (45) * iso_dist.x;// + fdf->mtrx.px_size.x; //* (fdf->mtrx.px_size.x * cos(30));
 		start.y += zoom_iter.x * sin(45) * iso_dist.y;
@@ -146,7 +103,6 @@ void	test_view_iso(t_fdf *fdf)
 	end.x = fdf->mtrx.end_draw.x + aux.x;
 	end.y = fdf->mtrx.start_draw.y + aux.y;
 	
->>>>>>> 9830de97052b839d1fb4868a4aea3da1fd34b158
 	while (seg_iter.x < fdf->mtrx.segments.x)
 //	while(seg_iter.x <= fdf->mtrx.px_size.x)
 	{
